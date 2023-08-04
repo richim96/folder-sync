b257d2c2eab2509b57480f5dcdff3d232b4aef1d
richim96 <mei.riccardo96@gmail.com>
2023-08-04 20:26:34 +0800

    refactor(sync.py): instantiate target folders and files

--- sync.py
+++ sync.py
@@ -1,20 +1,23 @@
 """Program entry point, to run the synchroniser.
 """
 
-from folder_sync import sync_log
-from folder_sync.sync_tools import run_sync
+from folder_sync import File, Folder
+
+# from folder_sync.sync_tools import run_sync
 
 LOGFILE_PATH_TEST = "tests/test_assets/log_file.csv"
 REPLICA_PATH_TEST = "tests/test_assets/replica_folder"
 SOURCE_PATH_TEST = "tests/test_assets/source_folder"
 
-source_hashes: list[str] = []
-replica_hashes: list[str] = []
-
 
 if __name__ == "__main__":
-    sync_log.info("Starting synchronisation...")
+    print("\n** | Starting synchronisation | **\n")
+
+    source_folder: Folder = Folder(SOURCE_PATH_TEST)
+    replica_folder: Folder = Folder(REPLICA_PATH_TEST)
+    sync_log_file: File = File(LOGFILE_PATH_TEST)
+
+    source_hashes: list[str] = []
+    replica_hashes: list[str] = []
 
     # TODO: Take arguments at the CLI level
-
-    run_sync()
