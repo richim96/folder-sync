89983800de47444676125f011cc944848d723e87
richim96 <mei.riccardo96@gmail.com>
2023-08-04 17:26:58 +0800

    test(tests): add test files for core modules

---
+++ tests/resources_test.py
@@ -0,0 +1,6 @@
+"""Tests for the _resources.py module.
+"""
+
+# import pytest
+
+# from folder_sync import _resources
---
+++ tests/sync_tools_test.py
@@ -0,0 +1,6 @@
+"""Tests for the sync_tools module.
+"""
+
+# import pytest
+
+# from folder_sync import sync_tools
